140e1c5a9ac5058e69fc96a4ba3bda464b2fdd71
Dlyted1 <mookathy3@gmail.com>
2023-09-18 14:09:02 -0600

    Magic Number Part 1 & 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+            if (number > guess) 
+            {
+               Console.Write("Higher"); 
+            }
+            else if (number < guess)
+            {
+               Console.Write("Lower");
+            }
+            else 
+            {
+                Console.Write("You guessed it!");
+            }
+ 
+
     }
 }
