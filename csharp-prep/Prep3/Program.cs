991a995aeca0243316b10c262cd3223fa1a5ed7f
Dlyted1 <mookathy3@gmail.com>
2023-09-19 11:50:39 -0600

    Part 1 and 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,30 +1,44 @@
 using System;
 
-class Program
+class program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
 
-        Console.Write("What is the magic number? ");
-        int number = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+Console.WriteLine("Hello Prep3 World!");
 
-            if (number > guess) 
-            {
-               Console.Write("Higher"); 
-            }
-            else if (number < guess)
-            {
-               Console.Write("Lower");
-            }
-            else 
-            {
-                Console.Write("You guessed it!");
-            }
- 
+// Part 1 and 2 
+Console.Write("What is the magic number? ");
+int number = int.Parse(Console.ReadLine());
 
+Random random = new Random();
+number = random.Next(1, 101);
+
+
+int guess = 0;
+
+while (guess != number)
+{
+
+
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+
+
+    if (number > guess)
+    {
+        Console.WriteLine("Higher");
     }
+    else if (number < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("You guessed it!");
+    }
+   }
+ }
 }
