dd70b61e252e79c7ded22d5f020f1363e9c0d010
Dlyted1 <mookathy3@gmail.com>
2023-09-21 11:59:26 -0600

    Part 3 Max number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -24,19 +24,23 @@ class Program
             }
         } while (userNumber != 0);
             
-           // Part 1: the sum of user entered numbers
+           // Part 1: Compute the sum, or total, of the numbers in the list
         int sum = 0;
         foreach (int number in numbers)
         {
             sum += number;
         }
 
-          // Part 2: average of the entered numbers
+          // Part 2: Compute the average of the numbers in the list
           // By making one of the variables a float first, the computer knows that it has to
-        // do the floating point division, and we get the decimal value that we expect.
+          // do the floating point division, and we get the decimal value that we expect.
           float average = (float)sum / numbers.Count;
+
+          // Part 3: Find the maximum, or largest, number in the list
+          int maxNumber = numbers.Count > 0 ? numbers.Max() : 0;
 
             Console.WriteLine($"The sum is :  {sum}");
             Console.WriteLine($"The average is :  {average}");
+            Console.WriteLine($"The largest number is:  {maxNumber}");
         }
 }
