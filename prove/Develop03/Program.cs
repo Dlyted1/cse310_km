4cf72006b7a10f63c92263a669fac08c9e852e6a
Kathy Moore <mookathy3@gmail.com>
2023-10-14 03:44:17 -0400

    Scripture Study

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
+namespace ScriptureStudy
+{
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture ("Ephesians ", 5, 20,  "Giving thanks always for all things unto God and the Father in the name of our Lord Jesus Christ. ");
+        Console.Clear();
+        scripture.Display();
+        Console.WriteLine("\nPress Enter to continue or type 'Quit' to exit.");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "Quit")
+                break;
+            
+            if (!scripture.HideRandomWord())
+            {
+                Console.WriteLine("\nNice job! You memorized the scripture!");
+                break;
+            }
+
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'Quit' to exit.");
+        }
     }
+}
 }
